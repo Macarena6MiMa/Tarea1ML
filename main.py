b6911f9e6810fc2512114f701a749821a1940313
Macarena6MiMa <mmino21@alumnos.utalca.cl>
2025-04-14 15:54:13 +0000

    gráfico de correlación

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 mpg = sns.load_dataset("mpg")
 sns.relplot(x="horsepower", y="mpg", hue="origin", size="weight",
@@ -11,3 +13,17 @@ g.map(sns.scatterplot, "horsepower", "mpg", s=100, alpha=.5)
 g.add_legend()
 
 plt.show()
+
+
+
+sns.set_theme(style="white")
+rs = np.random.RandomState(33)
+d = pd.DataFrame(data=rs.normal(size=(100, 6)),  # Changed to 6 columns
+                 columns=list("origin"))
+
+corr = d.corr()
+mask = np.triu(np.ones_like(corr, dtype=bool))
+f, ax = plt.subplots(figsize=(11, 9))
+cmap = sns.diverging_palette(230, 20, as_cmap=True)
+sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
