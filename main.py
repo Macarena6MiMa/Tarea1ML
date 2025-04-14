f5171be737b0c1922e4272709423870802be94bd
Macarena6MiMa <mmino21@alumnos.utalca.cl>
2025-04-14 15:19:03 +0000

    Gráfico de dispersión

---
+++ main.py
@@ -0,0 +1,9 @@
+import seaborn as sns
+mpg = sns.load_dataset("mpg")
+sns.relplot(x="horsepower", y="mpg", hue="origin", size="weight",
+            sizes=(40, 400), alpha=.5, palette="muted",
+            height=6, data=mpg)
+pal = dict(usa="purple", japan="blue", europe="green")  
+g = sns.FacetGrid(mpg, hue="origin", palette=pal, height=5)
+g.map(sns.scatterplot, "horsepower", "mpg", s=100, alpha=.5)
+g.add_legend()
